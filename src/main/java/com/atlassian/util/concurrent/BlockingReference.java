c2461f3430b2b685f61b1be33f232b6e47bcf466
Jed Wesley-Smith <jwesleysmith@atlassian.com>
2008-08-11 08:20:16 +0000

    javadoc improvements.

--- src/main/java/com/atlassian/util/concurrent/BlockingReference.java
+++ src/main/java/com/atlassian/util/concurrent/BlockingReference.java
@@ -22,47 +22,99 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Condition;
 
 /**
- * A Reference with queue semantics where rather than getting the current
- * reference it is taken instead. Analagous to a single element
- * {@link BlockingQueue} but does not support that whole interface.
+ * A Reference with queue semantics where rather than getting the current reference it is taken instead. Analogous to a single element
+ * {@link BlockingQueue}.
+ * <p>
+ * Note: this class does not support null elements being {@link #set(Object)} and will throw an exception. If the internal reference is null, then
+ * calls to {@link #take()} or {@link #take(long, TimeUnit)} will block.
+ * <p>
+ * This class is most suited to SRSW usage. Multiple writers will overwrite each other's elements, and if multiple readers are waiting to take a
+ * value, one reader will be arbitrarily chosen (similar to {@link Condition#signal()}).
  * 
- * @param <T>
+ * @param <V>
+ *            the value type
+ * 
+ * @see BlockingQueue
  */
-public class BlockingReference<T> {
-    private final AtomicReference<T> ref = new AtomicReference<T>();
+public class BlockingReference<V> {
+    private final AtomicReference<V> ref = new AtomicReference<V>();
     private final BooleanLatch latch = new BooleanLatch();
 
-    // public T take() throws InterruptedException {
-    // latch.await();
-    // return ref.getAndSet(null);
-    // }
-
-    public T take() throws InterruptedException {
+    /**
+     * Takes the current element if it is not null and replaces it with null. If the current element is null then wait until it becomes non-null.
+     * <p>
+     * If the current thread:
+     * <ul>
+     * <li>has its interrupted status set on entry to this method; or
+     * <li>is {@linkplain Thread#interrupt interrupted} while waiting,
+     * </ul>
+     * then {@link InterruptedException} is thrown and the current thread's interrupted status is cleared.
+     * 
+     * @return the current element
+     * @throws InterruptedException
+     *             if the current thread is interrupted while waiting
+     */
+    public V take() throws InterruptedException {
         while (true) {
             latch.await();
-            final T result = ref.getAndSet(null);
+            final V result = ref.getAndSet(null);
             if (result != null) {
                 return result;
             }
         }
     }
 
-    public T take(final long timeout, final TimeUnit unit) throws TimeoutException, InterruptedException {
+    /**
+     * Takes the current element if it is not null and replaces it with null. If the current element is null then wait until it becomes non-null. The
+     * method will throw a {@link TimeoutException} if the timeout is reached before an element becomes available.
+     * <p>
+     * If the current thread:
+     * <ul>
+     * <li>has its interrupted status set on entry to this method; or
+     * <li>is {@linkplain Thread#interrupt interrupted} while waiting,
+     * </ul>
+     * then {@link InterruptedException} is thrown and the current thread's interrupted status is cleared.
+     * 
+     * @param timeout
+     *            the maximum time to wait
+     * @param unit
+     *            the time unit of the {@code timeout} argument
+     * @return the current element
+     * @throws InterruptedException
+     *             if the current thread is interrupted while waiting
+     * @throws TimeoutException
+     *             if the timeout is reached without another thread having called {@link #set(Object)}.
+     */
+    public V take(final long timeout, final TimeUnit unit) throws TimeoutException, InterruptedException {
         if (!latch.await(timeout, unit)) {
             throw new TimeoutException();
         }
         return ref.getAndSet(null);
     }
 
-    public void set(final T value) {
+    /**
+     * Set the value of this reference. This method is lock-free. A thread waiting in {@link #take()} or {@link #take(long, TimeUnit)} will be
+     * released and given this value.
+     * 
+     * @param value
+     *            the new value.
+     */
+    public void set(final V value) {
         notNull("value", value);
         ref.set(value);
         latch.release();
     }
 
-    public boolean isNull() {
+    /**
+     * Whether or not the current value is null or not. If this is true and the next call to {@link #take()} or {@link #take(long, TimeUnit)} will not
+     * block.
+     * 
+     * @return true if the current reference is null.
+     */
+    public boolean isEmpty() {
         return ref.get() == null;
     }
 }
