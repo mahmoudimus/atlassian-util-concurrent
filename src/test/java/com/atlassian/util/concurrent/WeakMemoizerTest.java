cd731c8d9c984622fd3b002509c59cc73669b019
Jed Wesley-Smith <jwesleysmith@atlassian.com>
2011-05-04 04:06:19 +0000

    added test for different memoizers not interfering with each other, removed unnecessary test method prefixes

--- src/test/java/com/atlassian/util/concurrent/WeakMemoizerTest.java
+++ src/test/java/com/atlassian/util/concurrent/WeakMemoizerTest.java
@@ -1,7 +1,7 @@
 package com.atlassian.util.concurrent;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
@@ -23,47 +23,48 @@ public class WeakMemoizerTest {
     }
 
     @Test
-    public void testGetLock() throws Exception {
+    public void callingTwiceReturnsSame() throws Exception {
         final WeakMemoizer<Integer, String> memoizer = WeakMemoizer.weakMemoizer(lock());
-
-        final String one = memoizer.get(1);
-        assertEquals(one, memoizer.get(1));
+        assertSame(memoizer.get(1), memoizer.get(1));
     }
 
     @Test
-    public void testLockReferenceNotNull() throws Exception {
-        final String value = new String("value");
-        final MappedReference<String, String> ref = new MappedReference<String, String>("test", value, new ReferenceQueue<String>());
+    public void callingDifferentMemoizersReturnsDifferent() throws Exception {
+        assertNotSame(WeakMemoizer.weakMemoizer(lock()).get(1), WeakMemoizer.weakMemoizer(lock()).get(1));
+    }
+
+    @Test
+    public void lockReferenceNotNull() throws Exception {
+        final MappedReference<String, String> ref = new MappedReference<String, String>("test", new String("value"), new ReferenceQueue<String>());
         assertNotNull(ref.getDescriptor());
         assertNotNull(ref.get());
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testReferenceNullDescriptor() throws Exception {
+    public void referenceNullDescriptor() throws Exception {
         new MappedReference<String, String>(null, "value", new ReferenceQueue<String>());
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testReferenceNullValue() throws Exception {
+    public void referenceNullValue() throws Exception {
         new MappedReference<String, String>("ref", null, new ReferenceQueue<String>());
     }
 
     @Test
-    public void testMany() throws Exception {
+    public void many() throws Exception {
         final WeakMemoizer<Integer, String> memoizer = WeakMemoizer.weakMemoizer(lock());
 
         final int size = 10000;
         for (int i = 0; i < 10; i++) {
             System.gc();
             for (int j = 0; j < size; j++) {
-                final String one = memoizer.get(j);
-                assertSame(one, memoizer.get(j));
+                assertSame(memoizer.get(j), memoizer.get(j));
             }
         }
     }
 
     @Test
-    public void testLosesReference() throws Exception {
+    public void losesReference() throws Exception {
         final WeakMemoizer<Integer, String> memoizer = WeakMemoizer.weakMemoizer(lock());
 
         final WeakReference<String> one = new WeakReference<String>(memoizer.get(1));
