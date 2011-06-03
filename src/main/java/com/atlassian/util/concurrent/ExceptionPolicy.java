034b0f96bc9353001492933f5b20db1a9d6ba205
Ben Woskow <bwoskow@atlassian.com>
2011-06-03 16:38:06 +0000

    CONCURRENT-21 javadoc

--- src/main/java/com/atlassian/util/concurrent/ExceptionPolicy.java
+++ src/main/java/com/atlassian/util/concurrent/ExceptionPolicy.java
@@ -10,12 +10,19 @@ import static com.google.common.base.Functions.identity;
  * Default implementations can be found in {@link Policies}.
  */
 public interface ExceptionPolicy {
+    
+    /**
+     * Handle a supplier which may or may not throw an Exception.
+     *
+     * @param <T> the return type of the Supplier
+     * @return the ExceptionPolicy handler
+     */
     <T> Function<Supplier<T>, Supplier<T>> handler();
 
     /**
      * Default exception handling policies
      */
-    public enum Policies implements ExceptionPolicy {
+    enum Policies implements ExceptionPolicy {
         IGNORE_EXCEPTIONS {
             public <T> Function<Supplier<T>, Supplier<T>> handler() {
                 return Functions.<T> ignoreExceptions();
