d15fcb0335206a47f8dd3cc6d0cfde722a534d51
Jed Wesley-Smith <jed@atlassian.com>
2012-06-09 13:21:38 +1000

    added a serialVer to shut up the compiler warning

--- src/main/java/com/atlassian/util/concurrent/RuntimeExecutionException.java
+++ src/main/java/com/atlassian/util/concurrent/RuntimeExecutionException.java
@@ -8,6 +8,8 @@ import java.util.concurrent.ExecutionException;
  * Convenience class for re-throwing {@link ExecutionException}.
  */
 public class RuntimeExecutionException extends RuntimeException {
+    private static final long serialVersionUID = 1573022712345306212L;
+
     public RuntimeExecutionException(final ExecutionException cause) {
         super(notNull("cause", cause));
     }
