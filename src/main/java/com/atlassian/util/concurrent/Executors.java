4116cfa88420265a74ccf05bace58d1c2d1fbb0c
Jed Wesley-Smith <jed@atlassian.com>
2014-05-21 14:34:57 +1000

    https://bitbucket.org/atlassian/atlassian-util-concurrent/issue/6/add-an-interface-that-allows-submission-of add new interface for submission of Callables and Suppliers to an Executor

--- src/main/java/com/atlassian/util/concurrent/Executors.java
+++ src/main/java/com/atlassian/util/concurrent/Executors.java
@@ -1,9 +1,11 @@
 package com.atlassian.util.concurrent;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 
-public final class Executors {
+import com.google.common.util.concurrent.SettableFuture;
 
+public final class Executors {
   /**
    * {@link Executor} that limits the number submitted jobs to a fixed upper
    * bound, by blocking the producer thread on submission.
@@ -13,6 +15,60 @@ public final class Executors {
    */
   public static Executor limited(Executor delegate, int limit) {
     return new LimitedExecutor(delegate, limit);
+  }
+
+  /**
+   * {@link Executor} that limits the number submitted jobs to a fixed upper
+   * bound, by blocking the producer thread on submission.
+   * 
+   * @since 2.6.2
+   * @see LimitedExecutor for implementation details.
+   */
+  public static ExecutorSubmitter submitter(Executor delegate) {
+    return new DefaultSubmitter(delegate);
+  }
+
+  static class DefaultSubmitter implements ExecutorSubmitter {
+    private final Executor executor;
+
+    DefaultSubmitter(Executor executor) {
+      this.executor = executor;
+    }
+
+    @Override public void execute(Runnable command) {
+      executor.execute(command);
+    }
+
+    @Override public <T> Promise<T> submit(final Callable<T> callable) {
+      final CallableRunner<T> runner = new CallableRunner<T>(callable);
+      executor.execute(runner);
+      return runner.get();
+    }
+
+    @Override public <T> Promise<T> submit(Supplier<T> supplier) {
+      return submit(Suppliers.toCallable(supplier));
+    }
+
+    static class CallableRunner<T> implements Runnable, Supplier<Promise<T>> {
+      final Callable<T> task;
+      final SettableFuture<T> future = SettableFuture.create();
+
+      CallableRunner(Callable<T> task) {
+        this.task = task;
+      }
+
+      @Override public void run() {
+        try {
+          future.set(task.call());
+        } catch (Exception ex) {
+          future.setException(ex);
+        }
+      }
+
+      @Override public Promise<T> get() {
+        return Promises.forListenableFuture(future);
+      }
+    }
   }
 
   // /CLOVER:OFF
